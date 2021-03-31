e7e6664c342e64bf0441026cc22edc21af282628
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-03-31 14:34:04 -0300

    Util class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 const EntityBase = require('./EntityBase')
 
-const e = new EntityBase({
-    name:'Alex Ferreira',
-    gender: 'male',
-})
+const assert = require('assert')
+const Employee = require('./employee')
+const Util = require('./util')
+
+const GENDER = {
+    male: 'male',
+    female: 'female',
+}
+
+{
+    const employee = new Employee({
+        name:'Aline',
+        gender: GENDER.female
+    })
+    assert.throws(() => employee.birthYear, {message: 'You must define age first!'})
+}
+
+{
+    const employee = new Employee({
+        name:'Alex',
+        age: 18,
+        gender: GENDER.male    
+    })
+
+    assert.deepStrictEqual(employee.name, 'Mr. Alex')
+    assert.deepStrictEqual(employee.age, undefined)
+    assert.deepStrictEqual(employee.gender, undefined)
+    assert.deepStrictEqual(employee.grossPay, Util.formatCurrency(5000.40))
+}
